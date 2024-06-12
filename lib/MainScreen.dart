81d10b0d812dcbe34b0ec29dd8d54470c848194e
Sgale <97342601+Sgale952@users.noreply.github.com>
2024-06-12 10:39:15 +0700

    Delete unnecessary main

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -3,20 +3,6 @@ import 'package:OtoWave/RegistrationScreen.dart';
 import 'package:flutter/material.dart';
  // Импортируем экран регистрации
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(),
-      home: MainScreen(),
-    );
-  }
-}
-
 class MainScreen extends StatefulWidget {
   @override
   _MainScreenState createState() => _MainScreenState();
